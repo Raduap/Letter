0544b367ec1c3ac5f6de26ed2084ce050f9995f4
Raduap <43844930+Raduap@users.noreply.github.com>
2019-03-06 18:53:51 +0800

    Add files via upload

---
+++ src/main/java/project/letter/letter/letter.java
@@ -0,0 +1,19 @@
+package project.letter.letter;
+
+import android.app.Application;
+
+import skin.support.SkinCompatManager;
+import skin.support.app.SkinCardViewInflater;
+import skin.support.constraint.app.SkinConstraintViewInflater;
+import skin.support.design.app.SkinMaterialViewInflater;
+
+public class letter extends Application {
+    public  void onCreate() {
+        super.onCreate();
+        SkinCompatManager.init(this)
+                .addInflater(new SkinMaterialViewInflater())            // material design 控件换肤初始化[可选]
+                .addInflater(new SkinConstraintViewInflater())          // ConstraintLayout 控件换肤初始化[可选]
+                .addInflater(new SkinCardViewInflater())                // CardView v7 控件换肤初始化[可选]
+                .loadSkin();
+    }
+}
